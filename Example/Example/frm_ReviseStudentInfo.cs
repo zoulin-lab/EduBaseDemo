f8fce12ba8663e3f738608efbb6c889330b17975
3106869108@qq.com <3106869108@qq.com>
2020-11-10 23:07:16 +0800

    11.10更改

---
+++ Example/Example/frm_ReviseStudentInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SmartLinli.DatabaseDevelopement;
+
+namespace Example
+{
+    public partial class frm_ReviseStudentInfo : Form
+    {
+        private string StudentNo;   //学号
+        public frm_ReviseStudentInfo()
+        {
+            InitializeComponent();
+        }
+        public frm_ReviseStudentInfo(string studentNo) : this()   //将上一窗体的账号传入此窗体
+        {
+            this.StudentNo = studentNo;
+            string commandText =
+                $@"SELECT * FROM tb_StudentInformation WHERE No='{this.StudentNo}';";
+            SqlHelper sqlHelper = new SqlHelper();
+            sqlHelper.QuickRead(commandText);
+            if (sqlHelper.HasRecord)
+            {
+                this.txt_StudentNumber.Text = sqlHelper["No"].ToString();
+                this.txt_Name.Text = sqlHelper["Name"].ToString();
+                this.txt_PasswordProtectProblem1.Text = sqlHelper["PasswordProtectProblem_1"].ToString();
+                this.txt_PasswordProtectProblem2.Text = sqlHelper["PasswordProtectProblem_2"].ToString();
+                this.txt_Answer1.Text = sqlHelper["Answer_1"].ToString();
+                this.txt_Answer2.Text = sqlHelper["Answer_2"].ToString();
+            }
+        }
+        
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            string commandText=
+                $@"INSERT tb_User(PasswordProtectProblem_1, PasswordProtectProblem_2,Answer_1,Answer_2)
+                   VALUES
+                   ('{txt_PasswordProtectProblem1.Text.Trim()}', '{txt_PasswordProtectProblem2.Text.Trim()}'
+                    ,'{txt_Answer1.Text.Trim()}','{txt_Answer2.Text.Trim()}');";
+            SqlHelper sqlHelper = new SqlHelper();
+            int rowAffected/*受影响的行有几行*/= sqlHelper.QuickSubmit(commandText);
+            if (rowAffected == 1)
+            {
+                MessageBox.Show("重置成功！");
+            }
+            else
+            {
+                MessageBox.Show("重置失败！");
+            }
+            
+        }
+    }
+}
--- Example/Example/frm_SignUp.Designer.cs
+++ Example/Example/frm_SignUp.Designer.cs
@@ -33,6 +33,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btn_SignUp = new System.Windows.Forms.Button();
+            this.btn_Login = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txt_UserNumber
@@ -54,33 +55,45 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(51, 102);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.Size = new System.Drawing.Size(62, 18);
             this.label1.TabIndex = 2;
-            this.label1.Text = "账号";
+            this.label1.Text = "账号：";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(51, 165);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.Size = new System.Drawing.Size(62, 18);
             this.label2.TabIndex = 3;
-            this.label2.Text = "密码";
+            this.label2.Text = "密码：";
             // 
             // btn_SignUp
             // 
-            this.btn_SignUp.Location = new System.Drawing.Point(186, 221);
+            this.btn_SignUp.Location = new System.Drawing.Point(131, 226);
             this.btn_SignUp.Name = "btn_SignUp";
             this.btn_SignUp.Size = new System.Drawing.Size(75, 34);
             this.btn_SignUp.TabIndex = 4;
             this.btn_SignUp.Text = "注册";
             this.btn_SignUp.UseVisualStyleBackColor = true;
+            this.btn_SignUp.Click += new System.EventHandler(this.btn_SignUp_Click);
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(240, 226);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(75, 34);
+            this.btn_Login.TabIndex = 5;
+            this.btn_Login.Text = "登录";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
             // 
             // frm_SignUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(364, 285);
+            this.Controls.Add(this.btn_Login);
             this.Controls.Add(this.btn_SignUp);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -100,6 +113,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btn_SignUp;
+        private System.Windows.Forms.Button btn_Login;
     }
 }
 
